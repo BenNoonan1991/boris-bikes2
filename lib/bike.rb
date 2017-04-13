9c347058a30ac1ace8a6ff698781cded866dc97b
Adam Erdem Er <adamerdemer@gmail.com>
2017-04-13 16:04:09 +0100

    Added new bike features, red release, needs to be checked

--- lib/bike.rb
+++ lib/bike.rb
@@ -1,4 +1,18 @@
 class Bike
+  def report_broken
+    @broken = true
+  end
+
+  def broken?
+    @broken
+  end
+
+  def release_bike
+    fail "No bikes available" if broken?
+  end
+# Test is failing ^
+
+
   def working?
     true
   end
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -1,5 +1,19 @@
 require 'bike'
 
 describe Bike do
-it {is_expected.to respond_to :working?} # must check why rspec needs a symbol or in speech marks
+  it {is_expected.to respond_to :working?} # must check why rspec needs a symbol or in speech marks
+
+  it 'can be reported broken' do
+    subject.report_broken
+    expect(subject).to be_broken
+  end
+
+  it 'No bikes available' do
+    #subject.broken
+    expect { subject.release_bike }.to raise_error 'No bikes available'
+  end
+  # Test failing, make sure to check
+
+
+
 end
