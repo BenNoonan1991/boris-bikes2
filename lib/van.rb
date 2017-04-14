b0a38064beb5327c79a35e3eb10e31512c4bf8fd
BenNoonan1991 <ben.noonan2@gmail.com>
2017-04-14 17:41:48 +0100

    Adds send broken bikes test to vans spec

--- lib/van.rb
+++ lib/van.rb
@@ -1,4 +1,7 @@
 class Van
   attr_accessor :items_for_delivery
 
+  def drop_off_broken_bikes(broken_bikes)
+  end
+
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -4,4 +4,22 @@ describe Van do
 
   it { is_expected.to respond_to(:items_for_delivery) }
 
+  it { is_expected.to respond_to(:send_broken_bikes_to).with(1).argument }
+
+  describe 'send_broken_bikes_to' do
+    let(:bike) {Bike.new}
+    let(:initially_docked_bikes) {Array.new}
+    before(:example) do
+      5.times { bike.report_broken; subject.items_for_delivery.push(bike); initially_docked_bikes.push(bike) }
+      subject.send_broken_bikes_to(van)
+    end
+    it "sends @brokens_bikes array to van.items_for_delivery" do
+      expect(van.items_for_delivery).to eq initially_docked_bikes
+    end
+    it "removes bikes from @broken_bikes array after transfer to delivery vehicle" do
+      expect(subject.broken_bikes).to be_empty
+    end
+
+  end
+
 end
